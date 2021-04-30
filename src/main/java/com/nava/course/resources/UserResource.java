43354512366250f500f352a5225f1f1fd84c2c80
flino1934 <f.lino1934@hotmail.com>
2021-04-29 23:54:56 -0300

    User entity and resource

---
+++ src/main/java/com/nava/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.nava.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nava.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User u = new User(1L, "Felipe Lino", "f.lino1934@hotmail.com", "(13)98137 - 5682", "12345");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+}
